382549dd096f1bd755e158d217f9a77ed084478c
otabek-a <otabekabdurasulov245@gmail.com>
2025-04-18 15:51:01 +0500

    qwerty

--- main.py
+++ main.py
@@ -5,13 +5,28 @@ import wikipediaapi
 from googletrans import Translator
 from tinydb import TinyDB,Query
 import time
-import os
-token=os.getenv('TOKEN')
+import random
 chat = TinyDB('id.json')
+import requests
 wiki_wiki = wikipediaapi.Wikipedia(
     user_agent="MyTelegramBot/1.0 (contact: example@email.com)", 
     language="en"
 )
+def get_definition(word):
+    url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        try:
+            definition = data[0]['meanings'][0]['definitions'][0]['definition']
+            return definition
+        except KeyError:
+            return "No definition found."
+    else:
+        return "Error: Could not fetch data."
+
+
 
 conn = sqlite3.connect("students.db") 
 cursor = conn.cursor()
@@ -19,76 +34,49 @@ cursor = conn.cursor()
 def words(update, context):
     reply_key = [[ '➕ Add Words ✍️'],
                  ['📜 Show All Words 📚', '🌍 Show All Translations 🌐'],
-                 ['❌ Exit 🚪','delete topic'],
-                 
-                 ]
+                 ['❌ Exit 🚪','🗑️ Delete Topic 🧹']]
     key = ReplyKeyboardMarkup(reply_key, resize_keyboard=True)
-    update.message.reply_text("Please choose one of the options below ⬇️:", reply_markup=key)
+    update.message.reply_text("🧠💬 Please choose one of the options below ⬇️:", reply_markup=key)
+
 def delete(update,context):
-    update.message.reply_text("if you want to delete topic just send me like that !topic name")
+    update.message.reply_text("🗑️ If you want to delete a topic, just send me like that 👉 !topic_name 💥")
+
 def test(update, context):
     reply_key = [['🏁 Begin 🏃‍♂️', '📊 Show Result 📈'],     
                  ['❌ Exit 🚪']]
     key = ReplyKeyboardMarkup(reply_key, resize_keyboard=True)
-    update.message.reply_text("Please choose one of the options below ⬇️:", reply_markup=key)
+    update.message.reply_text("🎯💡 Please choose one of the options below ⬇️:", reply_markup=key)
+
 def begin_test(update,context):
-    reply_key = [['eng -- uzb ', '📊uzb--eng '],     
-                 ['definition -- eng '],['❌ Exit 🚪']]
+    reply_key = [['🧠 eng -- uzb ', '🧠 uzb -- eng '],     
+                 ['📘 definition -- eng '],['❌ Exit 🚪']]
     key = ReplyKeyboardMarkup(reply_key, resize_keyboard=True)
-    update.message.reply_text("please send me topic name after you want quiz for example #eng/topic_name , #uzb/topic_name, #def/topic_name⬇️:", reply_markup=key)
-def eng(update, context, text):
-    text = text.strip().lower()
-    user_id = update.message.from_user.id
-    table_name = 'a' + str(user_id)
+    update.message.reply_text("📢 Please send me topic name after you want quiz. For example: #eng/topic_name ⬇️")
 
-    conn = sqlite3.connect("students.db")
-    cursor = conn.cursor()
 
-    cursor.execute(f"SELECT 1 FROM {table_name} WHERE topic = ?", (text,))
-    result = cursor.fetchall()
-  
-    if len(result) >= 1:
-        update.message.reply_text(f'I found this topic "{text}" with {len(result)} words\n your test will begin if you want to stop pls send me -- stop() ')
-        time.sleep(1)
-        update.message.reply_text(f'5')
-        time.sleep(1.3)
-        update.message.reply_text(f'4')
-        time.sleep(1.5)
-        update.message.reply_text(f'3')
-        time.sleep(1.8)
-        update.message.reply_text(f'2')
-        time.sleep(2)
-        update.message.reply_text(f'1')
-        time.sleep(2.1)
-        update.message.reply_text(f"Begun ur test  for '{text}'")
-
-    else:
-        update.message.reply_text(f'I cannot find this topic. {text}')
-
-    
-  
 
 def add_data(update, context):
-    update.message.reply_text("Please send the topic name and word in the following format: topic_name*word ✍️")
+    update.message.reply_text("📝 Please send the topic name and word in the following format: topic_name*word ✍️")
 
 def start(update, context):
     Student = Query()
     chat_id = update.message.chat_id
     first_name = update.message.chat.first_name
     existing_user = chat.search(Student.chat_id == chat_id)
-   
+
     if not existing_user:
         chat.insert({'chat_id': chat_id})
+
     relpy_key =[['📚 Words 🏫', '👨‍🏫 TEST 🎓']]
     key = ReplyKeyboardMarkup(relpy_key)
     user_id = update.message.from_user.id
     table_name = 'a' + str(user_id)
     user_username = update.message.from_user.username
     if user_username:
-        update.message.reply_text(f"Hello, @{user_username}! 👋 To start using this bot, please choose one of the buttons below. 📲", reply_markup=key)
+        update.message.reply_text(f"👋 Hello, @{user_username}! To start using this bot, please choose one of the buttons below. 📲", reply_markup=key)
     else:
         user_first_name = update.message.from_user.first_name
-        update.message.reply_text(f"Hello, {user_first_name}! 👋 To start using this bot, please choose one of the buttons below. 📲", reply_markup=key)
+        update.message.reply_text(f"👋 Hello, {user_first_name}! To start using this bot, please choose one of the buttons below. 📲", reply_markup=key)
 
     conn = sqlite3.connect("students.db") 
     cursor = conn.cursor()
@@ -104,6 +92,8 @@ def start(update, context):
 
 def add_word(update, context, text):
     try:
+        word = text
+        definition = get_definition(word)
         user_id = update.message.from_user.id
         table_name = 'a' + str(user_id)
         matn = text.split('*')
@@ -126,7 +116,7 @@ def add_word(update, context, text):
             VALUES (?, ?, ?, ?)
         """, (topic_name, word, clean_definition, result.text))
         conn.commit() 
-        update.message.reply_text(f'{topic_name} is created topc and add this word {word}')
+        update.message.reply_text(f"✅ '{topic_name}' topic created and word '{word}' added successfully! 🎉")
     except Exception as e:
         update.message.reply_text(f"❌ Error occurred: {e}")
 
@@ -153,10 +143,44 @@ def show_words(update, context):
     for i, (topic, words) in enumerate(topics.items(), 1):
         message += f"\n📚 {i}. **Topic Name:** _{topic}_ ({len(words)} words):\n"
         for j, (word, definition) in enumerate(words, 1):
-            message += f"    {j}) {word} — {definition}\n"
+            message += f"    {j}) {word} — {definition} 📖\n"
         message += "━━━━━━━━━━━━━━━━━━━━━━\n"
 
     update.message.reply_text(message, parse_mode='Markdown')
+def eng(update, context, text):
+    
+    text = text.strip().lower()
+    user_id = update.message.from_user.id
+    table_name = 'a' + str(user_id)
+
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT 1 FROM {table_name} WHERE topic = ?", (text,))
+    result = cursor.fetchall()
+
+    if len(result) >= 1:
+        update.message.reply_text(f"✅ I found this topic \"{text}\" with {len(result)} words\n🧪 Your test will begin! If you want to stop, please send me -- stop() 🛑")
+        for count in [5, 4, 3, 2, 1]:
+            time.sleep(1)
+            update.message.reply_text(f"⏳ {count}")
+        update.message.reply_text(f"🚀 Let's begin your test for '{text}'! 💥")
+        cursor.execute(f"SELECT word FROM {table_name} WHERE topic = ?", (text,))
+        rows = cursor.fetchall()
+        word_list = [i[0] for i in rows]
+        i=1
+       
+        while len(word_list) > 0:
+              
+             word = random.choice(word_list)
+             update.message.reply_text(f"Qustion:{i}, What does '{word}' mean in english?")
+             word_list.remove(word)
+             i+=1
+             time.sleep(10)
+        update.message.reply_text('Test is stopped')
+        
+    else:
+        update.message.reply_text(f"⚠️ I cannot find this topic: {text} ❌")
 
 def show_uzbek(update, context):
     conn = sqlite3.connect("students.db")
@@ -181,10 +205,11 @@ def show_uzbek(update, context):
     for i, (topic, words) in enumerate(topics.items(), 1):
         message += f"\n📚 {i}. **Topic Name:** _{topic}_ ({len(words)} words):\n"
         for j, (word, uzbek) in enumerate(words, 1):
-            message += f"    {j}) {word} — {uzbek}\n"
+            message += f"    {j}) {word} — {uzbek} 🇺🇿\n"
         message += "━━━━━━━━━━━━━━━━━━━━━━\n"
 
     update.message.reply_text(message, parse_mode='Markdown')
+
 def delete_data(update,context,text):
     conn = sqlite3.connect("students.db")
     cursor = conn.cursor()
@@ -192,21 +217,21 @@ def delete_data(update,context,text):
     table_name = 'a' + str(user_id)
     cursor.execute(f"SELECT 1 FROM {table_name} WHERE topic = ?", (text,))
     result = cursor.fetchall()
-  
+
     if len(result) >= 1:
-        update.message.reply_text(f'I deleted this topic "{text}" with {len(result)} words')
+        update.message.reply_text(f"🗑️ Deleted topic \"{text}\" with {len(result)} words ❌")
         cursor.execute(f"DELETE FROM {table_name} WHERE topic = ?", (text,))
         conn.commit()
     else:
-        update.message.reply_text(f'I cannot find this topic: {text}')
-    
+        update.message.reply_text(f"⚠️ I cannot find this topic: {text} ❌")
 
 def check(update, context):
     global chat
     text = update.message.text.lower().strip()
+ 
     if text.startswith('!'):
-           text=text.replace('!','')
-           delete_data(update,context,text)
+        text=text.replace('!','')
+        delete_data(update,context,text)
     if text.startswith('*123'):
         message_to_send = text[4:].strip()
         if not message_to_send:
@@ -219,26 +244,26 @@ def check(update, context):
                 print(f"Error: {e}")
         update.message.reply_text("✅ Message sent to all users! 🚀")
         return
-  
     if text.startswith('#'):
-       parts = text[1:].split('/')
-       if len(parts) == 2:
-           lang, topic = parts
-           if lang == 'eng':
-            eng(update, context, topic)
+        parts = text[1:].split('/')
+        if len(parts) == 2:
+            lang, topic = parts
+            if lang == 'eng':
+                eng(update, context, topic)
+    
     if '*' in text:
         add_word(update, context, text)
+  
 
-    
-
-updater = Updater(token, use_context=True)
+updater = Updater('7981798770:AAGbSqQmu-Z4JJ5kD8P-wFwIIWaUvWmCOV4', use_context=True)
 dispatcher = updater.dispatcher
+
+
+
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(MessageHandler(Filters.text('📚 Words 🏫'), words))
 dispatcher.add_handler(MessageHandler(Filters.text('👨‍🏫 TEST 🎓'), test))
-
-
-dispatcher.add_handler(MessageHandler(Filters.text('delete topic'), delete))
+dispatcher.add_handler(MessageHandler(Filters.text('🗑️ Delete Topic 🧹'), delete))
 dispatcher.add_handler(MessageHandler(Filters.text('🏁 Begin 🏃‍♂️'), begin_test))
 dispatcher.add_handler(MessageHandler(Filters.text('🌍 Show All Translations 🌐'), show_uzbek))
 dispatcher.add_handler(MessageHandler(Filters.text('➕ Add Words ✍️'), add_data))
